26fed67d727ff6a4073f7c2e4bab7f09def83a11
Eternity1001 <MMirhossain01@gmail.com>
2024-04-05 07:53:20 -0400

    Added old working router

--- util/router.py
+++ util/router.py
@@ -1,31 +1,46 @@
 import re
+from util.request import Request
 
 
 class Router:
 
-    def __init__(self) -> None:
-        self.routeHolder = {}
+    def __init__(self):
+        self.routeHolder = []
 
     def add_route(self, httpMethod, path, method):
 
-        path = r'' + path
-        info = [path, method]
+        information = Information(httpMethod, path, method)
+        self.routeHolder.append(information)
 
-        if self.routeHolder.get(httpMethod):
-            listOfInfo = self.routeHolder[httpMethod]
-            listOfInfo.append(info)
-            self.routeHolder.update({httpMethod: listOfInfo})
-            return
-        self.routeHolder[httpMethod] = [info]
-        return
+    def route_request(self, request: Request):
 
-    def route_request(self, request):
-
-        if self.routeHolder.get(request.method):
-
-            for i in self.routeHolder[request.method]:
-                if len(i) == 2:
-                    if re.match(i[0], request.path):
-                        return i[1](request)
+        for i in self.routeHolder:
+            if i.compare(request):
+                return i.return_function_value(request)
 
         return b"HTTP/1.1 404 Not Found \r\nContent-Type: text/plain\r\nContent-Length: 36\r\nX-Content-Type-Options: nosniff \r\n\r\nThe Requested Content Does Not Exist"
+
+
+class Information:
+
+    def __init__(self, http, path, method):
+
+        self.http = http
+        self.path = path
+        self.method = method
+
+    def compare(self, request: Request):
+
+        regexHttp = re.compile(self.http)
+        regexPath = re.compile(self.path)
+
+        if regexHttp.match(request.method):
+            if regexPath.match(request.path):
+                return True
+        return False
+
+    def return_function_value(self, request):
+        return self.method(request)
+
+    def print_info(self):
+        return (self.http, self.path)
